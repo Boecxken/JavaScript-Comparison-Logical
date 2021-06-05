5bff351cb226c7e77f1e957bf6697678eebc5644
Boecxken <84395269+Boecxken@users.noreply.github.com>
2021-06-05 12:58:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+const age = 28;
+const isFemale = true;
+const driverStatus = "bob";
+const name = "Susanne";
+const totalAmount = 65;
+
+if (age >= 18) {
+
+    console.log("Kom maar binnen!");
+
+} else {
+
+    console.log("Je mag niet naar binnen!");
+
+}
+
+if (isFemale) {
+
+    console.log("Welkom!");
+
+} else {
+
+    console.log("Helaas!");
+
+}
+
+if (driverStatus === "bob") {
+
+    console.log("Je mag rijden!");
+
+} else {
+
+    console.log("Neem een taxi.");
+
+}
+
+if (age >= 18 && age <= 25) {
+
+    console.log("Je krijgt 50% korting!");
+
+} else {
+
+    console.log("Je krijgt geen korting.");
+
+}
+
+if (name === "Bram" || name === "Sarah") {
+
+    console.log("Gefeliciteerd, een gratis biertje!");
+
+} else {
+
+    console.log("Drie euro, alsjeblieft.");
+}
+
+if (totalAmount >= 100) {
+
+    console.log("Je krijgt een gratis fles champagne.");
+
+} else if (totalAmount >= 50) {
+
+    console.log("Je krijgt gratis een portie nacho's.");
+
+} else if (totalAmount >= 25) {
+
+    console.log("Je krijgt gratis bitterballen.");
+
+}
